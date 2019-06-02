7297712ee2f2cfcff03b554c61f745429d3c45ad
Huong <tmhuong@apcs.vn>
2019-06-02 23:53:32 +0700

    A02
    2D Animation and Scripting

---
+++ A02/Assets/myInstantiation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class myInstantiation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject myPrefab;
+    void Start()
+    {
+        Instantiate(myPrefab, new Vector2(0, 0), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
