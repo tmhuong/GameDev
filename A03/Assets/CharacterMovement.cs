e68cdc0e8c1b9df05463ece96e59ea07ae92d6db
Huong <tmhuong@apcs.vn>
2019-06-12 23:59:11 +0700

    A03
    2D Physics and Input System

---
+++ A03/Assets/CharacterMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public float speed = 1.0f;
+    public float jumpSpeed = 300f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(-speed, 0));
+            GetComponent<SpriteRenderer>().flipX = false;
+            GetComponent<Animator>().SetTrigger("StoneWalk");
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(speed, 0));
+            GetComponent<SpriteRenderer>().flipX = true;
+            GetComponent<Animator>().SetTrigger("StoneWalk");
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpSpeed));
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Destroy(collision.gameObject);
+        Debug.Log(collision.gameObject.name);
+    }
+}
---
+++ A03/Assets/KinematicControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KinematicControl : MonoBehaviour
+{
+    Vector2 newPos;
+
+    public float speed = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        newPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            newPos.x -= speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            newPos.x += speed;
+        }
+        transform.position = newPos;
+    }
+}
