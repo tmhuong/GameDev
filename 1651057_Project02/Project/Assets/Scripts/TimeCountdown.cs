899df055042a70240b03398be2fb5bc7db8df6f2
Huong <tmhuong@apcs.vn>
2019-09-05 23:57:38 +0700

    Final Project
    3D Games

---
+++ 1651057_Project02/Project/Assets/Scripts/TimeCountdown.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeCountdown : MonoBehaviour
+{
+    float currentTime = 0;
+    float staringTime = 100;
+
+    [SerializeField] Text timerText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = staringTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        timerText.text = currentTime.ToString("0");
+
+        if(currentTime <= 0)
+        {
+            currentTime = 0;
+        }
+    }
+}
---
+++ 1651057_Project02/Readme.md
@@ -0,0 +1,6 @@
+1651057 - Thai My Huong<br/>
+Final Project - 3D Games<br/><br/>
+
+Unity 2019.1.3f1 (64-bit)<br/><br/>
+
+Forest exploration, collecting objects within available time.
